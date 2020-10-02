4590a37e6c3630da2b643df97d51c1e91e64ea0b
ektatank <ektatank95@gmail.com>
2020-10-02 22:33:35 +0530

    folder name changed

--- backend/springboot/springboot-template/src/test/java/daiict/slop/springboot_starterproject/HealthControllerTest.java
+++ backend/springboot/springboot-template/src/test/java/daiict/slop/springboot_starterproject/HealthControllerTest.java
@@ -8,7 +8,6 @@ import org.mockito.InjectMocks;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,6 +33,7 @@ public class HealthControllerTest {
 
     /**
      * TestCase for Health API
+     *
      * @throws Exception
      */
     @Test
