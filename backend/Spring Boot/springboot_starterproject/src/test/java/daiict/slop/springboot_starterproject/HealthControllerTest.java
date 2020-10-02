e9a1e6016435053c2a5eae5e040087dc32e09305
Ekta Tank <201911005@daiict.ac.in>
2020-10-02 18:37:43 +0530

    Commited with  Unused Imports suggestion changes

--- backend/Spring Boot/springboot_starterproject/src/test/java/daiict/slop/springboot_starterproject/HealthControllerTest.java
+++ backend/Spring Boot/springboot_starterproject/src/test/java/daiict/slop/springboot_starterproject/HealthControllerTest.java
@@ -1,25 +1,20 @@
 package daiict.slop.springboot_starterproject;
 
 import daiict.slop.springboot_starterproject.controller.HealthController;
-import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- * HeallthController Tester.
+ * HealthController Tester.
  *
  * @author <Ekta Tank>
  * @version 1.0
