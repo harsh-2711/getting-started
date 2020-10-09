bdf35e9c256241f6bbec328d39a2cae81e04b204
Ekta Tank <201911005@daiict.ac.in>
2020-10-09 21:47:01 +0530

    chore:User Entity changed from Java to Kotlin

--- backend/springboot/springboot-template/src/main/java/daiict/slop/springboot_starterproject/entity/User.kt
+++ backend/springboot/springboot-template/src/main/java/daiict/slop/springboot_starterproject/entity/User.kt
@@ -1,63 +1,28 @@
-package daiict.slop.springboot_starterproject.entity;
+package daiict.slop.springboot_starterproject.entity
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.AllArgsConstructor
+import lombok.Data
+import lombok.NoArgsConstructor
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.Table
 
 /**
  * Database Entity-USER
  * @version 1.0
  * @since Oct 8, 2020
  */
-
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "\"User\"")
-public class User {
+class User {
     @Id
     @GeneratedValue
-    private int userId;
-    private String name;
-    private String phoneNumber;
-    private String emailAddress;
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
+    var userId = 0
+    var name: String? = null
+    var phoneNumber: String? = null
+    var emailAddress: String? = null
 }
