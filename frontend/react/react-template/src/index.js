4c8ca026aa4abc89ad097fd84105aa780756bc6a
myusername <ektatank95@gmail.com>
2020-10-18 11:04:07 +0530

    chore: updated import statements

--- frontend/react/react-template/src/index.js
+++ frontend/react/react-template/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import HelloWorld from './components/HelloWorld';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import HelloWorld from "./components/HelloWorld";
 
 ReactDOM.render(
   <React.StrictMode>
